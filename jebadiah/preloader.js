5021be89f1d302f160b35b5b8d78e96c3e5b7832
Frixolydian <mauricio.morales.arias93@gmail.com>
2016-06-17 11:28:02 -0500

    Preloader bar fix

--- jebadiah/preloader.js
+++ jebadiah/preloader.js
@@ -5,7 +5,7 @@ preloader.prototype = {
   },    
   preload:function(){
     this.add.image(this.game.width * 0.5, this.game.height * 0.75, 'preloader_back').anchor.set(0.5); 
-    this.loadingbar = this.add.sprite(120, 300, 'preloader_bar');
+    this.loadingbar = this.add.sprite(120, 292, 'preloader_bar');
     this.add.image(this.game.width * 0.5, this.game.height * 0.4, 'credits').anchor.set(0.5); 
     this.load.setPreloadSprite(this.loadingbar, 0);
 
