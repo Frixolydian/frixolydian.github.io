0c746d0a1f1b4004a251058b6e66a2453f846d6e
Frixolydian <mauricio.morales.arias93@gmail.com>
2019-05-16 13:54:26 -0500

    Chorus test again, no-load error fix

--- lofi/js/preload.js
+++ lofi/js/preload.js
@@ -21,7 +21,7 @@ for (var i = 1; i < 8; i++){
 			source: './sound/piano/A' + i +'.ogg',
 			detune: 100 * j,
 			Chorus : {
-			    intensity: 0.2,  //0 to 1
+			    intensity: 0.5,  //0 to 1
 			    rate: 4,         //0.001 to 8
 			    stereoPhase: 0,  //0 to 180
 			    bypass: 0
@@ -30,13 +30,13 @@ for (var i = 1; i < 8; i++){
 	}
 }
 
-for (var i = 1; i < 8; i++){
+for (var i = 1; i < 6; i++){
 	for (var j = 0; j < 12; j++){
 		keySamples['fmsynth'][(i - 1) * 12 + j] = new Wad({
 			source: './sound/fmsynth/A' + (i + 1) +'.ogg',
 			detune: 100 * j,
 			Chorus : {
-			    intensity: 0.2,  //0 to 1
+			    intensity: 0.5,  //0 to 1
 			    rate: 4,         //0.001 to 8
 			    stereoPhase: 0,  //0 to 180
 			    bypass: 0
